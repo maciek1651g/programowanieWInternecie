6de8b6595a94d914fd760a4866baf7c1b9d90747
Maciek <maciek1651g@gmail.com>
2021-03-17 13:39:48 +0100

    lab3_4

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const compression = require('compression');
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
+
+// Compress all HTTP responses
+app.use(compression())
 //Routing
-app.use(express.static('client'))
+app.use(express.static('client'));
 
  //Run server
  http.listen(port, function() {
